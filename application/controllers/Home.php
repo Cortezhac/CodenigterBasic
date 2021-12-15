0d9240fd6281ae4c51016b185d3449d4ff02f1ed
Irvin Cortez <irvincotez67@gmail.com>
2021-12-15 14:56:29 -0600

    libreria grocery CRUD

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -6,10 +6,13 @@ class Home extends My_Controller {
     function __construct()
     {
         parent::__construct();
+        $this->load->database();
     }
 
     public function index()
     {
+        $crud = new Grocery_CRUD();
+        $crud->set_table('alm_alumno');
         $this->layout_front('welcome_message', 'Bienvenido');
     }
 }
