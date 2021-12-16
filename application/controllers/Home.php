c8b398fbc440757902ae90addb5328a692dee8a7
Irvin Cortez <irvincotez67@gmail.com>
2021-12-16 09:13:29 -0600

    agregado  grocery crud

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -7,12 +7,33 @@ class Home extends My_Controller {
     {
         parent::__construct();
         $this->load->database();
+        $this->load->library('grocery_CRUD');
     }
 
     public function index()
     {
         $crud = new Grocery_CRUD();
         $crud->set_table('alm_alumno');
-        $this->layout_front('welcome_message', 'Bienvenido');
+        $crud->set_subject('Alumno', 'Alumnos');
+        $crud->columns(array(
+            'alm_codigo',
+            'alm_nombre',
+            'alm_edad',
+            'alm_sexo',
+            'alm_id_grd'
+        ));
+
+        $crud->display_as(array(
+            'alm_codigo' => 'Codigo',
+            'alm_nombre' => 'Nombre',
+            'alm_edad' => 'Edad',
+            'alm_sexo' => 'Sexo',
+            'alm_id_grd' => 'Grado'
+        ));
+
+        $crud->set_relation('alm_id_grd', 'grd_grado', 'grd_nombre');
+        $output = $crud->render();
+
+        $this->layout_front('example', 'Bienvenido', $output);
     }
-}
+} 
